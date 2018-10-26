f15f96993c93290f00059ba4fca0de963220c7e7
Justin Chin <justinmichaelchin@gmail.com>
2018-10-25 18:58:56 -0700

    adds manifest header file

---
+++ 362-p1_CLI/manifest.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+typedef std::vector<std::string> LabelList;
+typedef std::vector<std::vector<std::string> > FileList;  // [[filename1 artifact1 artifact2]]
+
+struct Manifest {
+  std::string command;
+  std::string timestamp;
+  std::string user;
+  LabelList labels;
+  FileList files;
+};
+
+
+std::string timestamp();
+Manifest LabelManifest(Manifest m, std::string label);
+void WriteManifestToDisk(Manifest m);
