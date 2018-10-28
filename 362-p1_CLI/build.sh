80788c20d5ad562765640ae00c20e2cf42c33903
Justin Chin <justinmichaelchin@gmail.com>
2018-10-28 14:41:13 -0700

    adds all compilation warnings and manifest-test to build.sh

--- 362-p1_CLI/build.sh
+++ 362-p1_CLI/build.sh
@@ -3,9 +3,13 @@
 # clean the directory
 rm -f *.out *.o *.s
 rm -f cli_vcs
+rm -f manifest-test
 
 # compile the programn
-g++ -std=c++11 -o cli_vcs versionControl.cpp -lboost_filesystem -lboost_system
+g++ -Wall -std=c++11 -o cli_vcs -lboost_filesystem -lboost_system versionControl.cpp
+
+# compile the manifest test file
+g++ -Wall -std=c++11 -o manifest-test -lboost_filesystem -lboost_system manifest.cpp
 
 # run the progam
 ./cli_vcs
