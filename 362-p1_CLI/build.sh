4ee0675d729f9b38b3854151417714e764eab680
Justin Chin <justinmichaelchin@gmail.com>
2018-11-03 18:03:09 -0700

    removes manifest.cpp from building separately

--- 362-p1_CLI/build.sh
+++ 362-p1_CLI/build.sh
@@ -6,10 +6,10 @@ rm -f cli_vcs
 rm -f manifest-test
 
 # compile the programn
-g++ -Wall -std=c++11 -o cli_vcs -lboost_filesystem -lboost_system versionControl.cpp
+g++ -Wall -std=c++11 -o cli_vcs -lboost_filesystem -lboost_system  manifest.cpp versionControl.cpp
 
 # compile the manifest test file
-g++ -Wall -std=c++11 -o manifest-test -lboost_filesystem -lboost_system manifest.cpp
+# g++ -Wall -std=c++11 -o manifest-test -lboost_filesystem -lboost_system manifest.cpp
 
 # run the progam
 ./cli_vcs
