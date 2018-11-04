04c2dde261e6193a3572345f9b781b03c6181433
Justin Chin <justinmichaelchin@gmail.com>
2018-11-04 11:42:31 -0800

    refactors build script
    adds compiler variable allowing for selection between g++ and clang++
    cleans up the command itself for legibility

--- 362-p1_CLI/build.sh
+++ 362-p1_CLI/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# set the compiler variable
+COMPILER=g++
+# COMPILER=clang++
+
 # clean the directory
 rm -f *.out *.o *.s
 rm -f cli_vcs
 
 # compile the programn
-g++ -Wall -std=c++11 -o cli_vcs -lboost_filesystem -lboost_system  manifest.cpp versionControl.cpp util.cpp repo.cpp
+$COMPILER -Wall \
+       -std=c++11 \
+       -o cli_vcs \
+       -lboost_filesystem \
+       -lboost_system \
+       manifest.cpp versionControl.cpp util.cpp repo.cpp
