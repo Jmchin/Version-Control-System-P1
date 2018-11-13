a93d3434646bc4e357d0360fc8609e7521f81ed7
Justin Chin <justinmichaelchin@gmail.com>
2018-11-13 12:31:47 -0800

    removes more uncessary code

--- 362-p1_CLI/manifest.cpp
+++ 362-p1_CLI/manifest.cpp
@@ -1,25 +1,5 @@
 #include "manifest.h"
 
-namespace fs = boost::filesystem;
-
-/* input: Manifest& m - the manifest object to write out
-          String path - the path to write the manifest object to
-
-   output: None
-
-   Side Effects: Writes the contents of Manifest m as a string to a file named
-                 by the path argument.
-*/
-void WriteManifestToPath(const Manifest& m, std::string path) {
-  std::ofstream file;
-
-  file.open(path);
-  file << ManifestToString(m);
-  file.close();
-
-}
-
-
 /* ============================================================================ */
 /*                              Auxililary Functions                            */
 /* ============================================================================ */
@@ -34,16 +14,6 @@ std::string timestamp() {
   // put_time outputs to a stream, so use a stringstream here
   std::stringstream buf;
   buf << std::put_time(&tm, "%F|%T");
-
-  return buf.str();
-
-}
-
-std::string ManifestToString(const Manifest& m) {
-  std::stringstream buf;
-  buf << m.command << "\n";
-  buf << m.timestamp << "\n";
-  buf << m.user << "\n";
 
   return buf.str();
 
--- 362-p1_CLI/manifest.h
+++ 362-p1_CLI/manifest.h
@@ -1,26 +1,10 @@
 #pragma once
 
-#include <iostream>
 #include <sstream>
 #include <ctime>
 #include <iomanip>
 #include <fstream>
-#include <vector>
 #include <string>
-#include <boost/filesystem.hpp>
-
-// TODO: Store the arguments used to operate the command (e.g Check-In
-// with project2/ as the source directory and repo2/ as the target
-// destination
-
-struct Manifest {
-  std::string command;
-  std::string timestamp;
-  std::string user;
-};
-
 
 std::string timestamp();
-std::string ManifestToString(const Manifest& m);
 void LogToManifest(std::string log, std::ofstream& manifest);
-void WriteManifestToPath(const Manifest& m, std::string path);
